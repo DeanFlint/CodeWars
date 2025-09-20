f41508e4ff2ee3fd30fa89da48f78c84017d7e53
Dean Flint <deanpf790@gmail.com>
2025-09-20 16:33:45 +0100

    ArrayDiff

---
+++ FilterTwoArrays.js
@@ -0,0 +1,18 @@
+/*
+Implement a function that computes the difference between two lists. 
+The function should remove all occurrences of elements from the first list (a) that are present in the second list (b). 
+The order of elements in the first list should be preserved in the result.
+
+Examples
+If a = [1, 2] and b = [1], the result should be [2].
+
+If a = [1, 2, 2, 2, 3] and b = [2], the result should be [1, 3].
+*/
+
+function array_diff(a, b) {
+  return a.filter(e => !b.includes(e));
+}
+
+const aSet = [1, 2, 3];
+const bSet = [1, 2];
+console.log(array_diff(aSet, bSet)); // Output: [3]
