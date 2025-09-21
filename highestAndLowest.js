f01bea11fbc3d1cfa8c65e22867032e421bd7fcb
Dean Flint <deanpf790@gmail.com>
2025-09-21 18:15:04 +0100

    highestAndLowest.js

---
+++ highestAndLowest.js
@@ -0,0 +1,52 @@
+/*
+In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+Examples
+
+highAndLow("1 2 3 4 5"); // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"
+
+Notes
+
+    All numbers are valid Int32, no need to validate them.
+    There will always be at least one number in the input string.
+    Output string must be two numbers separated by a single space, and highest number is first.
+*/
+
+/*
+Thought process:
+Split the string in an array
+Create a compare function to subtract b from a which will sort the array to ascending order
+Push the last item of the list and the first item of the list into a new array
+Join both items with a space inbetween.
+*/
+
+
+function highAndLow(numbers){
+  let numberList = numbers.split(" ");
+  let highestAndLowest = []
+
+  function compareNumbers(a, b) {
+    return a - b;
+  }
+
+  numberList.sort(compareNumbers)
+  highestAndLowest.push(numberList[numberList.length - 1], numberList[0]);
+
+  return highestAndLowest.join(" ");
+}
+
+
+console.log(highAndLow("1 2 3 4 5")); // return "5 1"
+console.log(highAndLow("1 2 -3 4 5")); // return "5 -3"
+console.log(highAndLow("1 9 3 4 -5")); // return "9 -5"
+console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4")); // return "42 -9"
+
+
+/*
+  Top Voted Solution:
+*/
+function highAndLow(numbers){
+  numbers = numbers.split(' ');
+  return Math.max.apply(0, numbers) + ' ' + Math.min.apply(0, numbers);
+}
